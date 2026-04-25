8ec3f96a38237b302b2e2cf452e42f14912e1dcf
Jasnoor Singh <81462656+Jasnoor22@users.noreply.github.com>
2026-04-25 23:50:56 +0530

    Add files via upload

---
+++ Q6.2.py
@@ -0,0 +1,13 @@
+def mm(x):
+    result =x**3
+    return resuly
+total = 0      
+num=int(input("Enter a num:"))
+for a in range(1,num):
+    total+= mm(a)
+    print(total)
+print("final total"total)
+    
+
+
+    
